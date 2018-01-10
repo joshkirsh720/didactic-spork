af7517425743bb986fbd2e04cdffb453652091fa
Josh Kirshenbaum <joshua.kirshenbaum@gmail.com>
2018-01-10 08:47:29 -0500

    holy lmao

--- Modified Bubble Sort/main.java
+++ Modified Bubble Sort/main.java
@@ -8,7 +8,8 @@ public class main {
         Comparison ascending = (a, b) -> a.compareTo(b) > 0;
         Comparison descending = (a, b) -> a.compareTo(b) < 0;
 
-        BubbleSort.sort(arr, descending);
+        BubbleSort.sort(arr, ascending);
+
 
         for(String a : arr) System.out.print(a + " ");
     }
