aefc31dc472df8d7092bd47ea6c8232ee37af3c6
Josh Kirshenbaum <joshua.kirshenbaum@gmail.com>
2018-01-12 08:11:22 -0500

    it woooooooooooorks

--- Modified Bubble Sort/bubblesort.java
+++ Modified Bubble Sort/bubblesort.java
@@ -1,3 +1,7 @@
+import java.util.stream.*;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.ArrayList;
 
 public class BubbleSort {
     public static void sort(Comparable[] arr, Comparison compare) {
@@ -15,6 +19,24 @@ public class BubbleSort {
                     count++;
                 }
             }
+        }
+
+        ArrayList<Comparable> vals = new ArrayList<Comparable>();
+        ArrayList<Integer> repeats = new ArrayList<Integer>();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(!vals.contains(arr[i])) {
+                vals.add(arr[i]);
+                repeats.add(1);
+            }
+            else {
+                int index = vals.indexOf(arr[i]);
+                repeats.set( index, repeats.get(index) + 1 );
+            }
+        }
+
+        for(int i = 0; i < vals.size(); i++) {
+            System.out.println(vals.get(i) + " is repeated " + repeats.get(i) + " times");
         }
     }
 }
